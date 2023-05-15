fbb199afd0409e0e78bffd155d33461e8edeacd9
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-05-15 12:33:27 +0530

    cards

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -2,12 +2,12 @@ import Image from 'next/image'
 import React from 'react'
 import Product from './Product'
 
-function ProductFeed({ products}) {
+function ProductFeed({ products }) {
   return (
-    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+    <div className="grid gap-2 grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto space-evenly px-10">
 
-      {products.slice(0,4).map(({ id, title, price, description, category, image }) => (
-        <Product 
+      {products.slice(0, 4).map(({ id, title, price, description, category, image }) => (
+        <Product
           key={id}
           id={id}
           title={title}
@@ -16,33 +16,33 @@ function ProductFeed({ products}) {
           category={category}
           image={image}
         />
-        ))}
-        
+      ))}
 
-        <div className="md:col-span-2">
-          {products.slice(4,5).map(({ id, title, price, description, category, image }) => (
-            <Product
-              key={id}
-              id={id}
-              title={title}
-              price={price}
-              description={description}
-              category={category}
-              image={image}
-            />
-          ))}
-        </div>
-        {products.slice(5,products.length).map(({ id, title, price, description, category, image }) => (
-            <Product
-              key={id}
-              id={id}
-              title={title}
-              price={price}
-              description={description}
-              category={category}
-              image={image}
-            />
-          ))}
+
+
+      {products.slice(4, 5).map(({ id, title, price, description, category, image }) => (
+        <Product
+          key={id}
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+        />
+      ))}
+
+      {products.slice(5, products.length).map(({ id, title, price, description, category, image }) => (
+        <Product
+          key={id}
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+        />
+      ))}
     </div>
   )
 }
